272f4f44c1f39a3b0be6d560e6ab0e425b45cf1d
Faris Hamam <faris.v.hamam@gmail.com>
2022-10-03 01:03:25 +0200

    add back ports to docker-compose, remove expose from Dockerfile, update App.js message

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Which port is this using? what does it matter? Removed ports 80:80 from docker-compose.yaml.
           Added EXPOSE 80 in docker file. This allows the container that is running on EBS AWS to be 
           to be reachable on port 80.
+          That did not work, so i added port mapping back to compose, and pushed again.
         </p>
         <a
           className="App-link"
