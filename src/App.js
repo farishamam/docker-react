f4c38f6de68e481c7a5fc59bd68677888fc9ccd6
Faris Hamam <faris.v.hamam@gmail.com>
2022-10-03 01:12:21 +0200

    update App.js message on new feature branch.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Which port is this using? what does it matter? Removed ports 80:80 from docker-compose.yaml.
-          Added EXPOSE 80 in docker file. This allows the container that is running on EBS AWS to be 
-          to be reachable on port 80.
-          That did not work, so i added port mapping back to compose, and pushed again.
+          This change was made on the newly created feature branch.
         </p>
         <a
           className="App-link"
